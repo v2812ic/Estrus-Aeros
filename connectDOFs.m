46ba9a6f45422759cad5f256a2ad026fbcb4be85
v2812ic <145837878+v2812ic@users.noreply.github.com>
2023-09-23 11:30:51 +0200

    Add files via upload

---
+++ connectDOFs.m
@@ -0,0 +1,23 @@
+function Td = connectDOFs(n_el,n_nod,n_i,Tn)
+%--------------------------------------------------------------------------
+% The function takes as inputs:
+%   - Dimensions:  n_el     Total number of elements
+%                  n_nod    Number of nodes per element
+%                  n_i      Number of DOFs per node
+%   - Tn    Nodal connectivities table [n_el x n_nod]
+%            Tn(e,a) - Nodal number associated to node a of element e
+%--------------------------------------------------------------------------
+% It must provide as output:
+%   - Td    DOFs connectivities table [n_el x n_el_dof]
+%            Td(e,i) - DOF i associated to element e
+%--------------------------------------------------------------------------
+
+Td = zeros(n_el,n_nod*n_i);
+    
+for i = 1 : n_nod
+    for j = 1 : n_i
+        Td(:,(i - 1)*n_i + j) = Tn(:,i)*n_i-(n_i-j);
+    end
+end
+
+end
